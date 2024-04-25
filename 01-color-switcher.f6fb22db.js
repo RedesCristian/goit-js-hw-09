993369f82eee6fcb2a27abd8fb2b46e89389db61
RedesCristian <149571424+RedesCristian@users.noreply.github.com>
2024-04-25 19:17:01 +0000

    Deploying to gh-pages from @ RedesCristian/goit-js-hw-09@58d6f0f4344ba1d3166cad8d93919469742c3bb4 🚀

---
+++ 01-color-switcher.f6fb22db.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o;const r=document.querySelector("body");t.addEventListener("click",(()=>{t.disabled=!0,o=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.f6fb22db.js.map
