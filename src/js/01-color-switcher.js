58d6f0f4344ba1d3166cad8d93919469742c3bb4
RedesCristian <149571424+RedesCristian@users.noreply.github.com>
2024-04-25 22:16:32 +0300

    ex 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let timerId;
+const body = document.querySelector('body');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timerId);
+});
